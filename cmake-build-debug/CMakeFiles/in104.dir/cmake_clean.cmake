f624036503db26b25acda8acfce52037607b4159
Xu-kexin <xkx@TABLET-LP96LC9M.localdomain>
2022-03-23 00:30:24 +0800

    ajout des fichiers initiaux

---
+++ cmake-build-debug/CMakeFiles/in104.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/in104.dir/main.cpp.o"
+  "in104"
+  "in104.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/in104.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/in104.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for in104.
+# This may be replaced when dependencies are built.
